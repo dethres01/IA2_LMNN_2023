b067cdc078c1e9f9c0076d4fce6b072014297fb3
Hirane <diego.preciado@alumno.udg.mx>
2023-11-23 17:35:21 -0600

    Deleted test prints, comments, code documentation will be done later by other people

--- main.py
+++ main.py
@@ -138,20 +138,15 @@ class MLN:
             self.epoch -= 1
             self.n_epoch += 1
 
-    def Loss(self, y, d):
-        return np.sum(np.power(d-y, 2))
-    def fd_Loss(self, y, d):
-        return 2*(d-y)
+
     def train_jacobian(self):
-        #we are going to try to calculate the jacobian matrix while training
         num_samples = len(self.X)
         jacobian_size = self.w_hidden.size + self.w_output.size
-        #print("jacobian size", jacobian_size)
+
         jacobian = np.zeros((num_samples, jacobian_size))
-        # going for a hard stop on epoch or error
+
         error = True
         self.X = np.matrix(self.X)
-        # weights already initialized
         lambda_factor = 0.1
         while self.epoch and error:
             error = False
@@ -159,137 +154,76 @@ class MLN:
             hidden_y, hidden_x, y = self.calc_hidden()
             # calculate error
             errors = np.array(self.d) - y
-            #print("errors", errors.shape)
             print("errors", errors)
 
             for n in range(num_samples):
-                #print("y[n]", np.array(y).flatten()[n])
-                #print("d[n]", self.d[n])
                 # jacobian matrix filling
                 #dL_dy = -2 * (self.d[n]-np.array(y).flatten()[n]) # this is dL/dy which is the derivative of the loss function with respect to output
                 dL_dy = -1
                 dy_dv = self.fd_activation(np.array(y).flatten()[n]) # this is dy/dv which is the derivative of the activation function with respect to the output
                 dv_dw = hidden_x[n] # this is dv/dw which is the derivative of the output with respect to the weights
-                #print("dl_dy", dL_dy.shape)
-                #print("dy_dv", dy_dv.shape)
-                #print("dv_dw", dv_dw.shape)
                 dL_dw = dL_dy*dy_dv*dv_dw# this is the derivative of the loss function with respect to the weights
-                #print("dl_dw", dL_dw.shape)
+
                 # jacobian matrix
                 jacobian[n, :self.w_output.size] = dL_dw.flatten()
-                #print("Hidden_y", hidden_y[n].flatten().T)
                 #hidden layer 
                 # we need dL_dy, dy_dv
 
                 dv_dh = np.transpose(self.w_output) # this is dv/dh which is the derivative of the output with respect to the hidden layer
                 # for each hidden neuron
-                #print(self.n_neurons)
                 for i in range(self.n_neurons):
 
-                    #print("hidden_y", hidden_y[n,i])
-                    #print("index", n,i)
 
                     dh_dvh = self.fd_activation(hidden_y[n,i])
-                    #print("dv_dh", dv_dh.shape)
-                    #print("dh_dvh", dh_dvh.shape)
                     dvh_dw = self.X[n]
-                    #print("dvh_dw", dvh_dw.shape)
+
                     dL_dw_hidden = dL_dy*dy_dv*dv_dh[i]*dh_dvh*dvh_dw
-                    #print("dL_dw_hidden", dL_dw_hidden.shape)
                     jacobian[n, self.w_output.size+i*self.w_hidden.shape[1]:self.w_output.size+(i+1)*self.w_hidden.shape[1]] = dL_dw_hidden.flatten()
 
-                #dh_dvh = self.fd_activation(np.array(hidden_y).flatten()[n]) # this is dh/dvh which is the derivative of the activation function with respect to the hidden layer
-                #dvh_dw = self.X[n] # this is dvh/dw which is the derivative of the hidden layer with respect to the weights
-
-                #print("dvh_dw", dvh_dw.shape)
-                
-
-                #print("dL_dw_hidden", dL_dw_hidden.shape)
-                #print("jacobian", jacobian.shape)
-                #print("jacobian", jacobian)
-            #update weights
-            # Weights update = (J^T* lambda Factor*Identity)^-1*J^T*error
-            print("jacobian", jacobian)
 
             jacobian_T = np.transpose(jacobian)
 
             identity = np.identity(jacobian_T.shape[0])
-            #print("identity", identity.shape)
 
             gradient_descent = lambda_factor*identity
-            #print("gradient_descent", gradient_descent.shape)
 
             hessian = np.dot(jacobian_T, jacobian) + gradient_descent
 
-            #print("hessian", hessian.shape)
-
             hessian_inv = np.linalg.inv(hessian)
 
-            #print("hessian_inv", hessian_inv.shape)
 
             delta =  np.dot(np.dot(hessian_inv, jacobian_T), errors.T)
             delta_1d = np.array(delta).flatten()
             #print("delta1d", delta_1d.shape)
 
-            # save a copy of weights that isn't updated by changes in self.w_output and self.w_hidden
             temp_w_output = self.w_output.copy()
             temp_w_hidden = self.w_hidden.copy()
-            #print("delta", delta[self.w_output.size:].shape)
-            #print("w_hidden", self.w_hidden)
+
             square_error = np.average(np.power(errors, 2))
-            #print("self.w_output", self.w_output.shape)
-            #print("delta", delta[:self.w_output.size].shape)
-            #print("self.w_output", self.w_output.shape)
-            #reshape delta to a n_neurons/input_size matrix
-            # split the rest of delta into n_neurons matrices of input_size
-            # TIME TO USE A COUNTER BECAUSE WE ARE DESPERATE
+
 
             counter = 0
-            #output layer
+
             for i in range(self.w_output.size):
-            #    print("delta value", delta_1d[i])
-            #    print("self.w_output[i]", self.w_output[i])
+
                 self.w_output[i] = self.w_output[i] - delta_1d[i]
-            #    print("self.w_output[i] after", self.w_output[i])
+
                 counter += 1
-        
-
-            
-            #print("self.w_output", self.w_output)
-            #temp
-            #print("temporary w_output", temp_w_output)
-
-            #print("temporary w_hidden", temp_w_hidden)
-            
-            #print("counter", counter)
             for i in range(self.n_neurons):
-            #    print("self.w_hidden[i]", self.w_hidden[i])
-                for j in range(self.X.shape[1]):
-            #        print("w_hidden[i,j]", self.w_hidden[i,j])
-                    self.w_hidden[i,j] = self.w_hidden[i,j] - delta_1d[counter]
-            #        print("w_hidden[i,j] after", self.w_hidden[i,j])
-                    counter += 1
-            #print("self.w_hidden", self.w_hidden)
-            
 
-            # check new error
+                for j in range(self.X.shape[1]):
+
+                    self.w_hidden[i,j] = self.w_hidden[i,j] - delta_1d[counter]
+
+                    counter += 1
             hidden_y, hidden_x, y_new = self.calc_hidden()
             errors = np.array(self.d) - y_new
-            print("errors", errors)
             square_error_new = np.average(np.power(errors, 2))
-            
-            print("square_error", square_error)
-            print("square_error_new", square_error_new)
             if square_error_new > square_error:
                 square_error = square_error_new
                 lambda_factor *= 1.1
             else:
-                print("Is lower")
                 lambda_factor *= 0.9
-
-            
-            #print("square_error", square_error)
-            print("lambda_factor", lambda_factor)
 
             if square_error > self.min_error:
                 error = True
@@ -303,7 +237,6 @@ class MLN:
 
     def plotting_jacob(self):
         self.ax.cla()
-        # predict
         _, _, y = self.calc_hidden()
 
         for i in range(len(np.array(y).flatten())):
@@ -314,7 +247,6 @@ class MLN:
 
         x_v = np.linspace(-4, 4, 20)
         y_v = np.linspace(-4, 4, 20)
-        # meshgrid
         meshX, meshY = np.meshgrid(x_v, y_v)
         meshZ = []
         for i in range(len(meshX)):
